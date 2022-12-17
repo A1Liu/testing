38893cbffabe37ea7f7b4cc8d7a7debbfce41886
Albert Liu <albertymliu@gmail.com>
2022-12-17 17:46:04 -0500

    meh

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { Entity, Column } from "@karimsa/tinyorm";
+import {Entity, Column, createSelectBuilder} from "@karimsa/tinyorm";
 import { createConnectionPool } from '@karimsa/tinyorm';
 
 const pool = createConnectionPool({
-
+  user: 'postgres',
+  password: 'password',
+  port: 1313,
 });
 
 export class User extends Entity({ schema: "app", tableName: "users" }) {
@@ -15,4 +17,48 @@ export class User extends Entity({ schema: "app", tableName: "users" }) {
 }
 
 const app = express();
-app.get("/", (req, res) => {});
+app.get("/users/:userid", async (req, res) => {
+  const id = req.params.userid;
+  await pool.withClient(async client => {
+    const user = await createSelectBuilder()
+        .from(User)
+        .addWhere(where => where("id").Equals(id))
+        .selectAll()
+        .getOne(client);
+    if (!user) {
+      res.json({});
+      return;
+    }
+
+    res.json(user);
+  })
+  res.json();
+});
+
+const start = async () => {
+  const migrations = await pool.getMigrationQueries(User);
+
+  console.log("migrations:", JSON.stringify(migrations));
+  for (const mig of migrations) {
+    console.log("suggested migration: (reason: " + mig.reason + ")");
+    for (const q of mig.queries) {
+      console.log("   ", q.text, "\n");
+      console.log("   values:", q.values);
+
+    }
+    console.log();
+  }
+
+  await pool.executeMigration("generated migrations", migrations);
+
+
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    /* eslint-disable no-console */
+    console.log(`Listening: http://localhost:${port}`);
+    /* eslint-enable no-console */
+  });
+};
+
+start();
+
